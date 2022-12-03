54831d92c6a8dd390b56a59c8784eb51ed801507
Erick Jantsch <erickjantsch@gmail.com>
2022-12-03 00:35:11 +0000

    Primera licao de C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Licao_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" 1) Faca um progarama para ler dois valores inteiros, e depois mostrar na tela a\n" +
+                 " desses numros com uma mensagen explicativa, conforme exemplos.");
+
+            Console.WriteLine("Informe o numero de a:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o numero de b:");
+            Console.WriteLine();
+            int num2 = int.Parse(Console.ReadLine());
+            int soma = num1 + num2;
+            Console.WriteLine("A soma e: " + soma);
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------");
+        }
+    }
+}
